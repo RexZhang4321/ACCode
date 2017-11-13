0d6b20a749cc6b51d3ae0544d5e872ab278cad8d
rexz <rexzhang4321@gmail.com>
2017-11-13 11:21:08 -0500

    change indent

--- src/app/components/CreateProjectBtn.js
+++ src/app/components/CreateProjectBtn.js
@@ -5,46 +5,46 @@ const FormItem = Form.Item
 
 export default class CreateProjectBtn extends Component {
 
-    static propTypes = {
-        visible: PropTypes.bool,
-        openWizard: PropTypes.func,
-        onCancel: PropTypes.func,
-        onOk: PropTypes.func
-    }
+  static propTypes = {
+    visible: PropTypes.bool,
+    openWizard: PropTypes.func,
+    onCancel: PropTypes.func,
+    onOk: PropTypes.func
+  }
 
-    render(){
-        return (
-            <div>
-                <Button size={'large'} onClick={ this.props.openWizard }>Create</Button>
-                <Modal
-                    visible={ this.props.visible }
-                    bodyStyle={{
-                        height: 130,
-                        overflow: 'auto',
-                    }}
-                    onCancel={ this.props.onCancel }
-                    onOk={ this.props.onOk }
-                    title="Create a new project"
-                    okText="Create" 
-                    cancelText="Cancel"
-                >
-                    <div ref="dialogContent">
-                        <Form>
-                            <FormItem label="Project Name" labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
-                                <Input
-                                
-                                />
-                            </FormItem>
+  render() {
+    return (
+      <div>
+        <Button size={'large'} onClick={this.props.openWizard}>Create</Button>
+        <Modal
+          visible={this.props.visible}
+          bodyStyle={{
+            height: 130,
+            overflow: 'auto',
+          }}
+          onCancel={this.props.onCancel}
+          onOk={this.props.onOk}
+          title="Create a new project"
+          okText="Create"
+          cancelText="Cancel"
+        >
+          <div ref="dialogContent">
+            <Form>
+              <FormItem label="Project Name" labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
+                <Input
 
-                            <FormItem label="Description" labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
-                                <Input
-                                
-                                />
-                            </FormItem>
-                        </Form>
-                    </div>
-                </Modal>
-            </div>
-        )
-    }
+                />
+              </FormItem>
+
+              <FormItem label="Description" labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
+                <Input
+
+                />
+              </FormItem>
+            </Form>
+          </div>
+        </Modal>
+      </div>
+    )
+  }
 }
