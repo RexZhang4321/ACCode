b0d1bc44f10b621ce6a82b0b5d10ab5dfeb2d5d8
rexz <rexzhang4321@gmail.com>
2017-11-15 00:14:43 -0500

    move SSE to Toolbar

--- src/app/containers/ACCodeApp.js
+++ src/app/containers/ACCodeApp.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 import CodeEditor from './CodeEditor'
 import Explorer from './Explorer'
 import OutputWindow from '../components/OutputWindow'
 import Toolbar from './Toolbar'
-import { finishBuildProject } from '../reducers/toolbar'
 import { Row, Col } from 'antd';
 
 class ACCodeApp extends Component {
-
-  static propTypes = {
-    finishBuildProject: PropTypes.func
-  }
-
-  componentWillMount() {
-    this._initServerSideEvent()
-  }
-
-  _initServerSideEvent() {
-    const url = 'http://localhost:5000/subscribeServer?project=' + 'test-android-hello'
-    const source = new EventSource(url)
-    source.onmessage = (e) => {
-      const response = JSON.parse(e.data)
-      switch (response.action) {
-        case 'build-finished':
-          this.props.finishBuildProject()
-        default:
-          console.log('no matching event!')
-      }
-    }
-  }
 
   render() {
     return (
@@ -50,19 +25,4 @@ class ACCodeApp extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {}
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    finishBuildProject: () => {
-      dispatch(finishBuildProject())
-    }
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ACCodeApp)
+export default ACCodeApp
--- src/app/containers/Toolbar.js
+++ src/app/containers/Toolbar.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import Toolbar from '../components/Toolbar'
-import { fireBuildProject } from '../reducers/toolbar'
+import { fireBuildProject, finishBuildProject } from '../reducers/toolbar'
 
 class ToolbarContainer extends Component {
   static propTypes = {
     buildProject: PropTypes.func,
+    finishBuildProject: PropTypes.func,
     isBuilding: PropTypes.bool
+  }
+
+  componentWillMount() {
+    this._initServerSideEvent()
+  }
+
+  _initServerSideEvent() {
+    const url = 'http://localhost:5000/subscribeServer?project=' + 'test-android-hello'
+    const source = new EventSource(url)
+    source.onmessage = (e) => {
+      const response = JSON.parse(e.data)
+      switch (response.action) {
+        case 'build-finished':
+          this.props.finishBuildProject()
+        default:
+          console.log('no matching event!')
+      }
+    }
   }
 
   openEmulator() {
@@ -40,6 +59,9 @@ const mapDispatchToProps = (dispatch) => {
   return {
     buildProject: (projectName) => {
       dispatch(fireBuildProject(projectName))
+    },
+    finishBuildProject: () => {
+      dispatch(finishBuildProject())
     }
   }
 }
